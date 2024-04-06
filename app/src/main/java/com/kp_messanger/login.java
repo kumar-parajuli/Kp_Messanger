8c04a9ace97841319e28c4b4104352fea4ea6790
parajuli rahul <parajulirahul98@gmail.com>
2024-04-06 21:36:02 +0545

    connect with firebasedatabase with firebase skd,cloudstoragesdk,firebaseauthentication & loginactivity

--- app/src/main/java/com/kp_messanger/login.java
+++ app/src/main/java/com/kp_messanger/login.java
@@ -1,6 +1,5 @@
 package com.kp_messanger;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,29 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
-    //check the database
-    FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        //check the user is login or not if login then see homepage other wise redirect to login page
-        auth = FirebaseAuth.getInstance();
-        if(auth.getCurrentUser() == null){
-            Intent intent = new Intent(MainActivity.this,login.class);
-            startActivity(intent);
-        }
-
-
-
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/kp_messanger/splash.java
+++ app/src/main/java/com/kp_messanger/splash.java
@@ -1,6 +1,8 @@
 package com.kp_messanger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,17 @@ public class splash extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
