01f815ec82008245b4b871cbf327350c1eaad600
parajuli rahul <parajulirahul98@gmail.com>
2024-04-07 22:54:13 +0545

    creating ui for signup and implement the logic for userloginwit authenticationwith database & refactorthe styleof both login,splash,and signuppage

---
+++ app/src/main/java/com/kp_messanger/registration.java
@@ -0,0 +1,68 @@
+package com.kp_messanger;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class registration extends AppCompatActivity {
+    TextView loginbut;
+    EditText rg_username, rg_email , rg_password, rg_repassword;
+    Button rg_signup;
+    CircleImageView rg_profileImg;
+    FirebaseAuth auth;
+    Uri imageURI;
+    String imageuri;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+
+loginbut= findViewById(R.id.signupbutton);
+rg_username =findViewById(R.id.rgusername);
+rg_email=findViewById(R.id.rgemail);
+rg_password =findViewById(R.id.rgpassword);
+rg_repassword=findViewById(R.id.rgpassword);
+
+// for login button
+        loginbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(registration.this,login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
