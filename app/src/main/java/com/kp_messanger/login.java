c987af6630963815db1accf960a1de151761b101
parajuli rahul <parajulirahul98@gmail.com>
2024-04-08 11:47:56 +0545

    refectoring the code logic for login & creating thelogic for signup to store data infirebase & uSERS class for communicated with database, implement the animation of top/button amimation

--- app/src/main/java/com/kp_messanger/login.java
+++ app/src/main/java/com/kp_messanger/login.java
@@ -1,5 +1,6 @@
 package com.kp_messanger;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -19,13 +20,30 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import android.widget.TextView;
+
 
 
 public class login extends AppCompatActivity {
+    // Declaration of UI elements
+    TextView logsignup;
     Button button;
     EditText email, password;
+
+
+    // Firebase authentication instance
     FirebaseAuth auth;
+
+
+    // Regular expression pattern for email validation
+
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+
+    // Progress dialog to show loading indicator
+
+    android.app.ProgressDialog progressDialog;
 
 
     @Override
@@ -34,11 +52,32 @@ public class login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        // Initialize progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please Wait...");
+        progressDialog.setCancelable(false);
 
+        // Initialize Firebase authentication instance
         auth = FirebaseAuth.getInstance();
+
+        // Find UI elements by their IDs
         button = findViewById(R.id.logbutton);
         email = findViewById(R.id.editTexLogEmail);
         password = findViewById(R.id.editTextLogPassword);
+        logsignup = findViewById(R.id.logsignup);
+
+
+        // Set click listener for signup text view
+        logsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,registration.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // Set click listener for login button
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,26 +85,35 @@ public class login extends AppCompatActivity {
                 String Email = email.getText().toString();
                 String pass = password.getText().toString();
 
-                //checking the email  field & password
+                //checking the Input validation
 
                 if ((TextUtils.isEmpty(Email))) {
+                    progressDialog.dismiss();
 
                     Toast.makeText(login.this, "Enter The Email", Toast.LENGTH_SHORT).show();
                 } else if (TextUtils.isEmpty(pass)) {
+                    progressDialog.dismiss();
+
                     Toast.makeText(login.this, "Enter The Password", Toast.LENGTH_SHORT).show();
                 } else if (!Email.matches(emailPattern)) {
+                    progressDialog.dismiss();
 
                     email.setError("Give Proper Email Address");
                 } else if (password.length() < 6) {
+                    progressDialog.dismiss();
 
                     password.setError("More Then Six Characters");
                     Toast.makeText(login.this, "Password Needs To Be Longer Then Six Characters", Toast.LENGTH_SHORT).show();
                 } else {
+                    // Sign in with Firebase authentication
                     auth.signInWithEmailAndPassword(Email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
+                                progressDialog.show();
+
                                 try {
+                                    // Redirect to MainActivity upon successful login
                                     Intent intent = new Intent(login.this, MainActivity.class);
                                     startActivity(intent);
                                     finish();
@@ -73,6 +121,7 @@ public class login extends AppCompatActivity {
                                     Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             } else {
+                                // Display error message if login fails
                                 Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -80,7 +129,7 @@ public class login extends AppCompatActivity {
                 }
             }
         });
-
+        // Edge-to-edge handling for UI layout
                 ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
                     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
                     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/kp_messanger/registration.java
+++ app/src/main/java/com/kp_messanger/registration.java
@@ -1,63 +1,220 @@
 package com.kp_messanger;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class registration extends AppCompatActivity {
+    // UI elements
     TextView loginbut;
-    EditText rg_username, rg_email , rg_password, rg_repassword;
+    EditText rg_username, rg_email, rg_password, rg_repassword;
     Button rg_signup;
     CircleImageView rg_profileImg;
-    FirebaseAuth auth;
     Uri imageURI;
     String imageuri;
+
+
+    // Regular expression for email validation
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+
+    // Firebase instances
+    FirebaseAuth auth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+
+    // Progress dialog to show loading indicator
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
+        // Initialize progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Establishing The Account");
+        progressDialog.setCancelable(false);
 
-loginbut= findViewById(R.id.signupbutton);
-rg_username =findViewById(R.id.rgusername);
-rg_email=findViewById(R.id.rgemail);
-rg_password =findViewById(R.id.rgpassword);
-rg_repassword=findViewById(R.id.rgpassword);
+        // Initialize Firebase instances
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
 
-// for login button
+        // Find UI elements by their IDs
+        loginbut = findViewById(R.id.loginbut);
+        rg_username = findViewById(R.id.rgusername);
+        rg_email = findViewById(R.id.rgemail);
+        rg_password = findViewById(R.id.rgpassword);
+        rg_repassword = findViewById(R.id.rgpassword);
+        rg_profileImg = findViewById(R.id.profilerg0);
+        rg_signup = findViewById(R.id.signupbutton);
+
+
+        // Click listener for the login button
         loginbut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(registration.this,login.class);
+                Intent intent = new Intent(registration.this, login.class);
                 startActivity(intent);
                 finish();
             }
         });
 
+        // Click listener for the signup button
+        rg_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve user input data
+                String namee = rg_username.getText().toString();
+                String emaill = rg_email.getText().toString();
+                String Password = rg_password.getText().toString();
+                String cPassword = rg_repassword.getText().toString();
+                String status = "Hey I'm Using Kumar Chat Application";
 
+                //checking user enter   Input validation
+                if (TextUtils.isEmpty(namee) || TextUtils.isEmpty(emaill) ||
 
+                        TextUtils.isEmpty(Password) || TextUtils.isEmpty(cPassword)) {
+                    progressDialog.dismiss();
 
+                    Toast.makeText(registration.this, "Please Enter Valid Information", Toast.LENGTH_SHORT).show();
+                } else if (!emaill.matches(emailPattern)) {
+                    progressDialog.dismiss();
 
+                    rg_email.setError("Please Type A Valid Email Here !");
+                } else if (Password.length() < 6) {
+                    progressDialog.dismiss();
 
+                    rg_password.setError("Password Must Be 6 Characters Or More ! ");
+                } else if (!Password.equals(cPassword)) {
+                    progressDialog.dismiss();
+
+                    rg_password.setError("Oops! Password Doesn't Match");
+                } else {
+
+                    // Create a new user in Firebase authentication
+                    auth.createUserWithEmailAndPassword(emaill, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Get the user ID
+                                String id = task.getResult().getUser().getUid();
+                                DatabaseReference reference = database.getReference().child("user").child(id);
+                                StorageReference storageReference = storage.getReference().child("Upload").child(id);
+
+                                // Upload profile image to Firebase storage if provided
+                                if (imageURI != null) {
+                                    // Get the download URL of the uploaded image
+                                    storageReference.putFile(imageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                    @Override
+                                                    public void onSuccess(Uri uri) {
+                                                        imageuri = uri.toString();
+
+                                                        // Create a new user object
+                                                        Users users = new Users(id, namee, emaill, Password, imageuri, status);
+                                                        // Store user data in Firebase database
+                                                        reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    progressDialog.dismiss();
+                                                                    // Redirect to the MainActivity upon successful registration
+                                                                    Intent intent = new Intent(registration.this, MainActivity.class);
+                                                                    startActivity(intent);
+                                                                    finish();
+                                                                } else {
+                                                                    Toast.makeText(registration.this, "Error while creating the user", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    // If profile image is not provided, use a default image URI
+                                    String status = "Hey I'm Using Kumar Chat Application";
+                                    imageuri = "https://firebasestorage.googleapis.com/v0/b/kp-messanger-e85fe.appspot.com/o/kp4.jpg?alt=media&token=c029f7da-2038-4b3b-827e-bc25db28dfc2";
+                                    Users users = new Users(id, namee, emaill, Password, imageuri, status);
+                                    reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                progressDialog.dismiss();
+                                                // Redirect to MainActivity upon successful registration
+                                                Intent intent = new Intent(registration.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            } else {
+                                                Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                }
+                            } else {
+                                // Display error message if user creation fails
+                                Toast.makeText(registration.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        // Set click listener for selecting a profile image from the gallery
+        rg_profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 10);
+            }
+        });
+    }
+    // Handle the result of selecting a profile image from the gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10) {
+            if (data != null) {
+                imageURI = data.getData();
+                rg_profileImg.setImageURI(imageURI);
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
