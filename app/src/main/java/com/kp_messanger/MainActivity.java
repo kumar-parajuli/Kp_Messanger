67b0d5d2eb393f481cb7f964b40a72a0e44b331d
parajuli rahul <parajulirahul98@gmail.com>
2024-04-09 00:45:19 +0545

    addingthe logout layout UI and logout logic,creatinuser_itemUi ad retreving the data fromdatabase

--- app/src/main/java/com/kp_messanger/MainActivity.java
+++ app/src/main/java/com/kp_messanger/MainActivity.java
@@ -1,25 +1,44 @@
 package com.kp_messanger;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     //check the database
     FirebaseAuth auth;
-
+    RecyclerView mainUserRecyclerView;
+    UserAdpter adapter;
+    FirebaseDatabase database;
+    ArrayList<Users> usersArrayList;
+    ImageView imglogout;
+    ImageView cumbut, setbut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +47,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //check the user is login or not if login then see homepage other wise redirect to login page
+        database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
-        if(auth.getCurrentUser() == null){
-            Intent intent = new Intent(MainActivity.this,login.class);
-            startActivity(intent);
-        }
 
-////Get reference to textview
-//        TextView textView = findViewById(R.id.textViewMessage);
-//// Check if dark mode is enabled
-//        boolean isDarkMode = isDarkModeEnabled();
-//        // If dark mode is enabled, set text color to a lighter color
-//        if (isDarkMode) {
-//            textView.setTextColor(ContextCompat.getColor(this, R.color.light_text_color));
-//        } else {
-//            // If dark mode is not enabled, keep the text color as it is (black)
-//            textView.setTextColor(ContextCompat.getColor(this, android.R.color.black));
-//        }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        DatabaseReference reference = database.getReference().child("user");
+        usersArrayList = new ArrayList<>();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    usersArrayList.add(users);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
-    }
-//    private boolean isDarkModeEnabled() {
-//        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        return sharedPreferences.getBoolean("dark_mode_enabled", false);
-//    }
-}
+        //Logout id getting
+        imglogout = findViewById(R.id.logoutimg);
+
+
+       //logout functionality with diglog box
+
+        imglogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(MainActivity.this, R.style.dialoge);
+                dialog.setContentView(R.layout.dialog_layout);
+                Button no, yes;
+                //foryes button functionality
+                yes = dialog.findViewById(R.id.yesbnt);
+                no = dialog.findViewById(R.id.nobnt);
+                yes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(MainActivity.this, login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                //for yes button on diaglog
+                no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+                mainUserRecyclerView = findViewById(R.id.mainUserRecyclerView);
+                mainUserRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+                adapter = new UserAdpter(MainActivity.this, usersArrayList);
+                //set the adaptar
+                mainUserRecyclerView.setAdapter(adapter);
+                if (auth.getCurrentUser() == null) {
+                    Intent intent = new Intent(MainActivity.this, login.class);
+                    startActivity(intent);
+                }
+
+
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+                });
+            }
+
+        }
